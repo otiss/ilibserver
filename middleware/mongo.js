e15c4da95c6f2edc600fb328198d36dcf51942be
Otis <otis.shou@gmail.com>
2014-08-19 23:52:08 +0800

    mongo middleware

--- middleware/mongo.js
+++ middleware/mongo.js
@@ -1,63 +1,14 @@
-var mongo = require("mongodb"),
-	MongoClient = mongo.MongoClient;
-var config,
-	db;
-
-exports.init = function(conf){
-    config = conf.db;
-}
-
-var func_connect = function(success, fail){
-	MongoClient.connect('mongodb://' + config.host + ':' + config.port + '/' + config.name, function(err, database) {
-	  if(err) return fail && fail(err);
-	  db = database;
-	  success && success();
-	});
-}
-
-exports.open = function(callback){
-    if(db){
-    	callback && callback(db);
-    }else{
-    	func_connect(function(){
-    		 callback && callback(db);
-    	});
-    }
-    
-}
-
-exports.db = function(success, fail){
-    if(db){
-        success(db);
-    }else{
-        func_connect(function(){
-            success(db);
-        }, function(err){
-            fail(err);
-        });
-    }
-}
-
-exports.collection = function(name, success, fail){
-    exports.db(function(db){
-        db.collection(name, function (err, collection) {
-            if(err) return fail(err);
-            success(collection,db);
-        });
-    }, function(err){
-        fail(err);
-    });
-}
-
+var mongodb = require("../lib/mongo")();
 
 exports.middleware = function(name){
     return function(req, res, next){
-        exports.collection(name, function(collection, db){
+        mongodb.collection(name, function(err, collection, db){
+            if(err){
+                return next(err);
+            }
             req.db = db;
             req.collection = collection;
             next();
-        }, function(err){
-            next(err);
-        })
+        });
     }
 }
